5c1c846660b1321775d1072bfc3e399bf7dfee1c
thomasmartinez114 <tmartinez114@gmail.com>
2020-04-19 03:14:54 -0400

    added html of the site to .get() it and imported requirment

--- imdb/src/main/java/Scraper.java
+++ imdb/src/main/java/Scraper.java
@@ -1,6 +1,11 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Scraper {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        final Document document = Jsoup.connect("https://www.imdb.com/chart/top/").get();
     }
 }
