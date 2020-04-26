461a7d9d675d5ac102ef8ffe17647adeb3b25141
Thomas Martinez <tmartinez114@gmail.com>
2020-04-26 00:35:46 -0400

    parse the title to console

--- stock-market/src/Demoscrape.java
+++ stock-market/src/Demoscrape.java
@@ -1,4 +1,12 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Demoscrape {
-
+	public static void main(String[] args) {
+		String html = "<html><head><title>First parse</title></head>"
+				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
+				Document doc = Jsoup.parse(html);
+				
+				System.out.println(doc.getElementsByTag("title"));
+	}
 }
