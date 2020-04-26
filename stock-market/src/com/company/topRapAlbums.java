ab3d3e970c11c2cafa59e046363b1c9251ee743f
Thomas Martinez <tmartinez114@gmail.com>
2020-04-26 19:16:23 -0400

    data not fetching

--- stock-market/src/com/company/topRapAlbums.java
+++ stock-market/src/com/company/topRapAlbums.java
@@ -11,8 +11,21 @@ public class topRapAlbums {
 	public static void main(String[] args) {
 		
 		// Site fetching from
-		final String url = "https://rateyourmusic.com/list/theu003/top-250-rap-albums-of-all-time/"
+		final String url = "https://rateyourmusic.com/list/theu003/top-250-rap-albums-of-all-time/";
 		
+		try {
+			final Document doc = Jsoup.connect(url).get();
+			
+			for (Element row : doc.select(
+					"table.user_list tr")) {
 				
+				final String artist = row.select(".list_artist").text();
+				
+				System.out.println("Artist: " + artist);
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
