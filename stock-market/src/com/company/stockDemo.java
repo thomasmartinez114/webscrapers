65efe7e725c2c02689fccb977a6b2ffc5a62a993
Thomas Martinez <tmartinez114@gmail.com>
2020-04-26 17:39:37 -0400

    successfully fetched IMDB Top 250 movies

--- stock-market/src/com/company/stockDemo.java
+++ stock-market/src/com/company/stockDemo.java
@@ -2,6 +2,7 @@ package com.company;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 // Tutorial
 // https://www.youtube.com/watch?v=tI1qGwhn_bs
@@ -10,11 +11,19 @@ public class stockDemo {
 	public static void main(String[] args) {
 		
 		// Site fetching from
-		final String url = "https://www.baseball-reference.com/teams/NYY/2019.shtml";
+		final String url = "https://www.imdb.com/chart/top/";
 		
 		try {
 			final Document doc = Jsoup.connect(url).get();
-			System.out.println(doc.outerHtml());
+			
+			for (Element row : doc.select(
+					"table.chart.full-width tr")) {
+				
+				final String title = row.select(".titleColumn").text();
+				final String rating = row.select(".imdbRating").text();
+				
+				System.out.println(title + " -> Rating: " + rating);
+			}
 		}
 		catch  (Exception ex) {
 			ex.printStackTrace();
