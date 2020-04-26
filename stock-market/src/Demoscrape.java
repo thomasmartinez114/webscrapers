5cf50af13bd2bad9917112c280bc410a946b2515
Thomas Martinez <tmartinez114@gmail.com>
2020-04-26 15:56:35 -0400

    fetch from a file

--- stock-market/src/Demoscrape.java
+++ stock-market/src/Demoscrape.java
@@ -1,12 +1,14 @@
 import java.io.IOException;
+import java.io.File;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 public class Demoscrape {
 	public static void main(String[] args) throws IOException {
-		Document doc = Jsoup.connect("http://example.com/").get();
+		File input = new File("C:\\Users\\tmartinez.MYIGT\\OneDrive - IGT PLC\\My Docs\\Azure Repos\\GNIE_build\\main\\index.html");
+		Document doc = Jsoup.parse(input, "UTF-8", "");
+		
 		System.out.println(doc);
 	}
 }
