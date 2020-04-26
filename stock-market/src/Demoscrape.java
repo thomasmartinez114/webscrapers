9d37c6f0121b04fdb3e649d89127ed6f0990323c
Thomas Martinez <tmartinez114@gmail.com>
2020-04-26 00:42:00 -0400

    added nodes.element and changed what parsing

--- stock-market/src/Demoscrape.java
+++ stock-market/src/Demoscrape.java
@@ -1,12 +1,12 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class Demoscrape {
 	public static void main(String[] args) {
-		String html = "<html><head><title>First parse</title></head>"
-				  + "<body><p>Parsed HTML into a doc.</p></body></html>";
-				Document doc = Jsoup.parse(html);
-				
-				System.out.println(doc.getElementsByTag("title"));
+		String html = "<div><p>Lorem Ipsum.</p>";
+		Document doc = Jsoup.parseBodyFragment(html);
+		Element body = doc.body();
 	}
 }
+ 
