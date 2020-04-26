67c4560e28617604dddb43552d216c0845532ce4
Thomas Martinez <tmartinez114@gmail.com>
2020-04-26 18:04:28 -0400

    began top rap albums scraper

---
+++ stock-market/src/com/company/topRapAlbums.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+// Tutorial
+// https://www.youtube.com/watch?v=tI1qGwhn_bs
+
+public class topRapAlbums {
+	public static void main(String[] args) {
+		
+		// Site fetching from
+		final String url = "https://rateyourmusic.com/list/theu003/top-250-rap-albums-of-all-time/"
+		
+				
+	}
+}
