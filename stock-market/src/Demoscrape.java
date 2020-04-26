72a68383485bca73918ce2fba0e1da70197400ea
Thomas Martinez <tmartinez114@gmail.com>
2020-04-26 15:51:35 -0400

    scraped content of the website

--- stock-market/src/Demoscrape.java
+++ stock-market/src/Demoscrape.java
@@ -1,12 +1,13 @@
+import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class Demoscrape {
-	public static void main(String[] args) {
-		String html = "<div><p>Lorem Ipsum.</p>";
-		Document doc = Jsoup.parseBodyFragment(html);
-		Element body = doc.body();
+	public static void main(String[] args) throws IOException {
+		Document doc = Jsoup.connect("http://example.com/").get();
+		System.out.println(doc);
 	}
 }
  
