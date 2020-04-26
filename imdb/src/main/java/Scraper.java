ab5b96e5cb755f09de8c3cdbd53811010d77f900
Thomas Martinez <tmartinez114@gmail.com>
2020-04-25 23:21:19 -0400

    began stock market scarper

--- imdb/src/main/java/Scraper.java
+++ imdb/src/main/java/Scraper.java
@@ -1,12 +1,10 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.IOException;
-
 public class Scraper {
 
     public static void main(String[] args) throws Exception {
-        final Document document = Jsoup.connect("https://www.imdb.com/chart/top/").get();
+        final Document document = Jsoup.connect("http://www.imdb.com/chart/top/").get();
 
         System.out.println(document.outerHtml());
     }
