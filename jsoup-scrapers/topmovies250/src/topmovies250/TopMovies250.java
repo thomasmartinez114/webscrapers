0168a3dc89c6ca0f1c79b440a806b92f8fcfeb46
Thomas Martinez <tmartinez114@gmail.com>
2020-04-27 20:22:48 -0400

    printed out entire site

--- jsoup-scrapers/topmovies250/src/topmovies250/TopMovies250.java
+++ jsoup-scrapers/topmovies250/src/topmovies250/TopMovies250.java
@@ -1,5 +1,22 @@
 package topmovies250;
 
-public class TopMovies250 {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+public class TopMovies250 {
+	public static void main(String[] args) {
+		
+		final String url = "https://www.imdb.com/chart/top/";
+		
+		try {
+			final Document doc = Jsoup.connect(url).get();
+			// Print out the whole get() of site
+			System.out.println(doc);
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+	}
 }
